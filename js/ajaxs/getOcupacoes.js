917428e57963bc8782c381cf105489e36086dc38
matheus <matheus.mmn@hotmail.com>
2022-04-28 09:02:30 -0300

    telas de relatórios

---
+++ js/ajaxs/getOcupacoes.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    dadosOcupacoes()
+})
+
+async function dadosOcupacoes(){
+
+    const resposta = await fetch("https://defmoteapi.herokuapp.com/ocupacoes/")
+    const dados = await resposta.json()
+    
+    var tabelaHomeOcupacoes = document.getElementById('tabelaHomeOcupacoes')
+    tabelaHomeOcupacoes.innerHTML = ''
+
+    dados.forEach(elemento => {
+        
+        var data = elemento.data
+        var codigo = elemento.codigo
+        var quarto = elemento.quarto
+        var entrada = elemento.entrada
+        var saida = elemento.saida
+        var total = elemento.total
+
+        tabelaHomeOcupacoes.innerHTML += '<tr>'+
+                                            '<td>' + data + '</td>'+
+                                            '<td>' + codigo + '</td>'+
+                                            '<td>' + quarto + '</td>'+
+                                            '<td>' + entrada + '</td>'+
+                                            '<td>' + saida + '</td>'+
+                                            '<td>' + total + '</td>'+
+                                        '</tr>'
+    });
+}
