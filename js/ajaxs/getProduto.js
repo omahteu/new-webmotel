2b54355d109c4780652af96da131b784978fe038
matheus <matheus.mmn@hotmail.com>
2022-05-10 08:22:48 -0300

    checkbox produtos

--- js/ajaxs/getProduto.js
+++ js/ajaxs/getProduto.js
@@ -2,26 +2,26 @@ $(document).ready(function(){
 
     produtoCodigo()
     
-    $.get("https://defmoteapi.herokuapp.com/produtos/", (resultado) => {
+    $.get("https://demomotelapi.herokuapp.com/produtos/", (resultado) => {
 
     resultado.forEach(function(item){
-        $('#cod').append('<option>' + item.descricao + '</option>');
+        $('#checkbox_produto').append('<option>' + item.descricao + '</option>');
     });
     
-    codProduto
+    //codProduto
 
-    $('#cod').change(function() {
-        var option = $('#cod').find(":selected").index()
+    $('#checkbox_produto').change(function() {
+        var option = $('#checkbox_produto').find(":selected").index()
 
         var db = option - 1
     
-        $("#des").val(resultado[db].descricao)
-        $("#vun").val('R$ ' + resultado[db].valorunitario)
+        $("#descricao").val(resultado[db].descricao)
+        $("#valor_unitario").val('R$ ' + resultado[db].valorunitario)
     
-        $('#qtd').keyup(function(){
+        $('#quantidade').keyup(function(){
             var qtd = $(this).val()
             var total = Number(resultado[db]['valorunitario']) * Number(qtd)
-            $("#tot").val('R$ ' + total)
+            $("#valor_total").val('R$ ' + total)
         });
     });
 })
