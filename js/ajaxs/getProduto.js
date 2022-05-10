2c4a958131c17bbc9ae543caaa371a90fde4d0f4
matheus <matheus.mmn@hotmail.com>
2022-05-10 08:26:38 -0300

    campos produtos

--- js/ajaxs/getProduto.js
+++ js/ajaxs/getProduto.js
@@ -8,8 +8,6 @@ $(document).ready(function(){
         $('#checkbox_produto').append('<option>' + item.descricao + '</option>');
     });
     
-    //codProduto
-
     $('#checkbox_produto').change(function() {
         var option = $('#checkbox_produto').find(":selected").index()
 
@@ -29,22 +27,22 @@ $(document).ready(function(){
 
 function produtoCodigo(){
 
-    $('#codProduto').keypress( (event) => {
+    $('#codigo_produto').keypress( (event) => {
 
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if(keycode == '13'){
 
-            $.get("https://defmoteapi.herokuapp.com/produtos/", (resultado) => {
+            $.get("https://demomotelapi.herokuapp.com/produtos/", (resultado) => {
 
                 var db = 0
     
-                $("#des").val(resultado[db].descricao)
-                $("#vun").val('R$ ' + resultado[db].valorunitario)
+                $("#descricao").val(resultado[db].descricao)
+                $("#valor_unitario").val('R$ ' + resultado[db].valorunitario)
             
-                $('#qtd').keyup(function(){
+                $('#quantidade').keyup(function(){
                     var qtd = $(this).val()
                     var total = Number(resultado[db]['valorunitario']) * Number(qtd)
-                    $("#tot").val('R$ ' + total)
+                    $("#valor_total").val('R$ ' + total)
                 });
             })
 
