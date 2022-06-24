2d52aa8c801ec7b1156f88f165ca4605ccc61c4e
matheus <matheus.mmn@hotmail.com>
2022-06-24 12:22:02 -0300

    numeracao automatica

--- js/cadastros/postQuarto.js
+++ js/cadastros/postQuarto.js
@@ -1,17 +1,32 @@
+$(document).ready( () => {
+    busca_ultimo_quarto()
+})
+
+var nu
+
+async function busca_ultimo_quarto(){
+    const query = await fetch("https://demomotelapi.herokuapp.com/quartos/")
+    const dados = await query.json()
+    dados.forEach(e => {
+        nu = Number(e.numero)
+    });
+
+}
+
 $("#salvarFormPostQuarto").click(function(){
     var codigoQuarto = $("#codigoQuarto").val()
-    var numeroQuarto = $("#numeroQuarto").val()
+    var numeroQuarto = nu + 1
     var percentual = $("#adicionarPercentualQuarto").val()
     var suite = $("#tipoSuiteQuarto").val()
     var tabela = $("#tabelaQuarto").val()
     var dados = {
         codigo: codigoQuarto,
-        numero: numeroQuarto,
+        numero: String(numeroQuarto),
         tipo_quarto: suite,
         tipo_tabela: tabela,
         percentual: percentual,
     }
-    $.post("https://demomotelapi.herokuapp.com/quartos/", dados, function(){
+    $.post("https://demomotelapi.herokuapp.com/quartos/", dados, () => {
         alert("Quarto Registrado!")
         document.getElementById('formCadastros').reset()
     })
