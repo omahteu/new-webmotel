88eae564c4278fc1ad2e5678abc5bb30aec0883b
matheus <matheus.mmn@hotmail.com>
2022-06-28 11:37:22 -0300

    periodo do dia

---
+++ js/style/periodo.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    saudacao()
+})
+
+function saudacao(){
+    var base = new Date()
+    var hora = base.getHours()
+    if(hora >= 0 && hora < 12){
+        $("#periodo").text("Bom dia")
+    } else if(hora >= 12 && hora <= 18){
+        $("#periodo").text("Boa tarde")
+    } else{
+        $("#periodo").text("Boa noite")
+    }
+}
