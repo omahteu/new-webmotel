851f32ea056872ddcc5a769f41f15f1da635580f
matheus <matheus.mmn@hotmail.com>
2022-06-29 01:56:47 -0300

    ajustes de caixa e propsta na home

--- js/setup/sitePath.js
+++ js/setup/sitePath.js
@@ -10,16 +10,19 @@ function historicoUrls(){
         $("#urlCadastrosCaixa").attr('href', './homecaixa.html')
         $("#urlQuartosCaixa").attr('href', './homecaixa.html')
         $("#urlCaixaCaixa").attr('href', './homecaixa.html')
+    } else if(info === 'admin'){
+        $("#home").attr('href', './home.html')
+        $("#urlCadastrosCaixa").attr('href', './home.html')
+        $("#urlQuartosCaixa").attr('href', './home.html')
+        $("#urlConfiguracoesAdmin").attr('href', './home.html')
     }
     if(status == 'aberto'){
         $("#abrirCaixa").css('display', 'none')
         $("#usarFundoCaixa").css('display', 'none')
         $("#fecharCaixa").css('display', 'inline')
-    }
-    if(info === 'admin'){
-        $("#home").attr('href', './home.html')
-        $("#urlCadastrosCaixa").attr('href', './home.html')
-        $("#urlQuartosCaixa").attr('href', './home.html')
-        $("#urlConfiguracoesAdmin").attr('href', './home.html')
+    } else if(status == "fechado"){
+        $("#abrirCaixa").css('display', 'inline')
+        $("#usarFundoCaixa").css('display', 'inline')
+        $("#fecharCaixa").css('display', 'none')
     }
 }
