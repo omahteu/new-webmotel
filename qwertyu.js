48c9f294d491da27de3bc18af0cdbbb1d3b4fd5a
matheus <matheus.mmn@hotmail.com>
2022-07-06 09:09:32 -0300

    testanddo

--- qwertyu.js
+++ qwertyu.js
@@ -1,15 +1,44 @@
 $(document).ready(function(){
-    var ds = das()
+    /*
+    das()
     console.log(qw)
 
-    var po = window.open()
-    for(var i = 0; i < qw.length; i++){
+    setTimeout( () => {
+        var po = window.open()
         po.document.write(
-            `<h1>${qw[i]}</h1>`
+            '<html>'+
+                '<body>'
         )
-    }
+    
+        po.document.write(
+            `<h1>${qw[0]}</h1>`
+        )
+        po.document.write(
+                '</body>'+
+            '</html>'
+        )
+    }, 2000)
+    */
+
+    
+    setTimeout( function(){
+        exportReportToExcel()
+    }, 2000)
+
+
 
 })
+
+function exportReportToExcel() {
+    let table = document.getElementsByID("relCartoes");
+    TableToExcel.convert(table[0], { 
+      name: `file.xlsx`,
+      sheet: {
+        name: 'Sheet 1'
+      }
+    });
+    console.log('doc criado')
+}
 
 var qw = []
 
