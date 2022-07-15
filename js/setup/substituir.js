32c68e798293aadd794baef3f3bb5b4ee785a910
matheus <matheus.mmn@hotmail.com>
2022-07-15 10:51:50 -0300

    ajustes

--- js/setup/substituir.js
+++ js/setup/substituir.js
@@ -3,15 +3,33 @@ import { link } from "../setup/index.js"
 $("#substituir").click(function(){
     $.get(link[5], function(e){
         var quarto = $("#quarto_antigo").val()
+        var novo = $("#quartos_disponiveis").val()
         var dados = e.filter(item => item.quarto === quarto)
         dados.forEach(el => {
             var id = el.id
+            // PATCH COMANDA
             $.ajax({
                 headers : {
                     'Accept' : 'application/json',
                     'Content-Type' : 'application/json'
                 },
                 url : link[5] + id + "/",
+                type : 'PATCH',
+                data : JSON.stringify({quarto: "2"}),
+                success : function() {
+                    console.log("Troca Com Sucesso!");
+                },
+                error : function(textStatus, errorThrown) {
+                    console.log(`ERRO: ${textStatus} - ${errorThrown}`)
+                }
+            })
+            // PATCH PÁTIO
+            $.ajax({
+                headers : {
+                    'Accept' : 'application/json',
+                    'Content-Type' : 'application/json'
+                },
+                url : link[15] + id + "/",
                 type : 'PATCH',
                 data : JSON.stringify({quarto: "2"}),
                 success : function() {
